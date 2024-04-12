07a51e7decb7fcaa25ddfaa71c58e4061d309fbf
Yelzhan04 <eljan00.kz@gmail.com>
2024-04-12 20:57:05 +0600

    first commit

---
+++ src/main/java/org/example/currencylimitmanager/CurrencyLimitManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.currencylimitmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyLimitManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyLimitManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/currencylimitmanager/CurrencyLimitManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.currencylimitmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyLimitManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
